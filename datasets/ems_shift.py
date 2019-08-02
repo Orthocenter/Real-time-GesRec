fe014e0661056bf6d9996b01d4cc25a53e8095be
Yuxin Chen <chenyuxin.mail@gmail.com>
2019-08-02 14:02:14 -0500

    15.35.1

--- datasets/ems_shift.py
+++ datasets/ems_shift.py
@@ -226,8 +226,8 @@ class EMS_shift(data.Dataset):
         self.init_data_paths()
         self.load_annotations()
         # self.random_offsets = [0] + [-i for i in range(1,self.random_offset+1)] + [i for i in range(1,self.random_offset+1)]
-        ## temporarily set to [1] for 15.34.11
-        self.random_offsets = [-1]
+        ## temporarily set to [0, -1, 1] for 15.35.1
+        self.random_offsets = [0, -1, 1]
     
     def find_class_id(self, ges):
         for name, i in self.class_to_idx.items():
